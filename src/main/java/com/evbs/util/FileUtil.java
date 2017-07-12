068ce2c72879b396eec3cb031dd300435b9c44f7
SquirrelWorkpace <827321394@qq.com>
2017-07-12 18:47:27 +0800

    Ninth Commit Use the common-io read and write file object

---
+++ src/main/java/com/evbs/util/FileUtil.java
@@ -0,0 +1,53 @@
+package com.evbs.util;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Created by squirrel-chen on 7/12/17.
+ */
+public class FileUtil {
+
+    public static int writeToFile(String filepath,String data)
+    {
+        File outputfile=new File(filepath);
+        try {
+            if(!outputfile.exists())
+            {
+                System.out.println("文件不存在");
+                outputfile.createNewFile();
+            }
+            FileUtils.writeStringToFile(outputfile,data);
+            return 1;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return 0;
+        }
+    }
+
+    public static String readFromFile(String filepath)
+    {
+        File inputfile=new File(filepath);
+        try{
+            if(!inputfile.exists())
+            {
+                System.out.println("文件不存在");
+                return "";
+            }
+           return FileUtils.readFileToString(inputfile,"UTF-8");
+        }
+        catch(IOException e)
+        {
+            e.printStackTrace();
+            return "";
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        FileUtil.writeToFile("/home/squirrel-chen/hello.txt","hello world 你是猪吗？？？ 123456");
+        System.out.println(FileUtil.readFromFile("/home/squirrel-chen/hello.txt"));
+    }
+}
